a6229b5c435226546cdcad8ce8c4fdfc0051d435
joseflores-dev <79271069+joseflores-dev@users.noreply.github.com>
2021-02-18 14:41:44 -0300

    Update settings.py

--- Pasteleria/settings.py
+++ Pasteleria/settings.py
@@ -129,7 +129,7 @@ SWEETIFY_SWEETALERT_LIBRARY = 'sweetalert2'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'botsaurio1@gmail.com' 
-EMAIL_HOST_PASSWORD = 'papeles16'
+EMAIL_HOST_USER = '' 
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
