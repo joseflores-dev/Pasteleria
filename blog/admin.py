a0a7616e6fb38de3e6c102ab2f0985ca1eeb2c04
ovahroot <josefloresover@gmail.com>
2021-02-18 13:03:34 -0300

    git remote set-url origin https://<joseflores-dev>@github.com/joseflores-dev/Pasteleria.git/

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from .models import Producto,Perfil,Reserva,Mensaje,Categoria
 # Register your models here.
 
-admin.site.register(Producto)
 admin.site.register(Perfil)
 admin.site.register(Reserva)
 admin.site.register(Mensaje)
